11f0ba99d3a95ef91fd1b19227c3cede1682490b
CanFam23 <nickclouse03@gmail.com>
2026-03-02 13:58:46 -0800

    Finish implementing GlobleService and add K-doc

--- src/main/kotlin/com/carroll/globle/service/GlobleService.kt
+++ src/main/kotlin/com/carroll/globle/service/GlobleService.kt
@@ -1,23 +1,44 @@
 package com.carroll.globle.service
 
+import com.fasterxml.jackson.module.kotlin.jacksonObjectMapper
 import jakarta.annotation.PostConstruct
 import net.sf.geographiclib.Geodesic
 import org.locationtech.jts.geom.Geometry
+import org.locationtech.jts.io.geojson.GeoJsonWriter
 import org.locationtech.jts.operation.distance.DistanceOp
 import org.springframework.stereotype.Service
 
+/** Meter to miles conversion factor */
 private const val M_TO_MILES = 0.0006213712
 
+/**
+ * This services contains the
+ * core functionality for the game.
+ * This includes:
+ * 1. Generating the target country
+ * 2. Computing distances between countries
+ * 3. Checking guesses against the target country
+ * 4. Returning data on each country in GeoJson format.
+ */
 @Service
 class GlobleGameService(
     private val datasetService: CountryDatasetService
 ) {
 
+    /** List of country names in the dataset, used for picking the target country. */
     private lateinit var countryNames: MutableList<String>
 
+    /** Current target country, initialized as "None" but is set to a country after dependencies are injected*/
     private var currentTarget: String = "None"
+
+    /** Distance each country is from the target country */
     private var targetDistances: Map<String, Int> = emptyMap()
 
+    /**
+     * Initializes the service after dependency injection.
+     *
+     * Initializes the country names list and generates the first target country.
+     */
     @PostConstruct
     fun init() {
         countryNames = datasetService.countriesByKey
@@ -26,9 +47,16 @@ class GlobleGameService(
 
         generateTargetCountry()
 
-        println("Current target is $currentTarget")
+        println("Current target is $currentTarget") // TODO REMOVE
     }
 
+    /**
+     * Generates a new target country. The new target country
+     * is the last element in the country names array. If its empty,
+     * its reinitialized and shuffled. After a new country is chosen,
+     * the `targetDistances` array is updated to contain the distances
+     * to the new target country.
+     */
     fun generateTargetCountry() {
         if (countryNames.isEmpty()) {
             countryNames = datasetService.countriesByKey
@@ -51,23 +79,34 @@ class GlobleGameService(
         currentTarget = newTargetName
     }
 
-    fun distanceMiles(a: Geometry, b: Geometry): Int {
-        if (a.intersects(b)) return 0
-
-        val pts = DistanceOp.nearestPoints(a, b)
-        val p1 = pts[0]
-        val p2 = pts[1]
-
-        val inv = Geodesic.WGS84.Inverse(p1.x, p1.y, p2.x, p2.y)
-        val meters = inv.s12
-
-        return (meters * M_TO_MILES).toInt()
-    }
-
+    /**
+     * Checks if the given guess is a country in the dataset and matches the
+     * current target country. Returns data on the guess country in geojson form.
+     *
+     * Format for the returned JSON:
+     *```
+     * {
+     *   "type": "Feature",
+     *   "id": country name,
+     *   "geometry": Geometry of country,
+     *   "properties": {
+     *      "correct": If country is the target,
+     *      "distance": Distance to target country,
+     *      "center": Rough center of countries polygon,
+     *      "country": Name of country
+     *    }
+     * }
+     * ```
+     * @param guess The guess to check
+     *
+     * @return GeoJSON data formatted like above.
+     */
     fun checkGuess(guess: String): Map<String, Any> {
         val cleanGuess = guess.trim().lowercase()
 
-        val countryData = datasetService.countriesByKey[cleanGuess] ?: return mapOf("error" to "Country '$guess' not recognized")
+        // Get country data or return error message
+        val countryData = datasetService.countriesByKey[cleanGuess] ?:
+                return mapOf("error" to "'$guess' is not a country in our database")
 
         // Center of shapes geometry, used to move map to the country
         // The centroid is like the area-weight center
@@ -76,15 +115,55 @@ class GlobleGameService(
         val geojson = mapOf(
             "type" to "Feature",
             "id" to guess,
-            "geometry" to countryData.second,
+            "geometry" to geometryToGeoJsonMap(countryData.second),
             "properties" to mapOf(
                 "correct" to (guess == currentTarget),
                 "distance" to targetDistances[guess],
-                "center" to center,
+                "center" to geometryToGeoJsonMap(center),
                 "country" to countryData.first,
             )
         )
 
         return geojson
+    }
+
+    /**
+     * Calculates the distance between two geometries. Returns their
+     * distance in miles, assuming their original CRS is in WGS84 / Lat and Long values.
+     *
+     * @param a: Geometry A
+     * @param b: Geometry B
+     *
+     * @return The distance between the two geometries in miles (truncated).
+     */
+    private fun distanceMiles(a: Geometry, b: Geometry): Int {
+        if (a.intersects(b)) return 0
+
+        // Nearest points on each geometry to each other
+        val pts = DistanceOp.nearestPoints(a, b)
+        val p1 = pts[0]
+        val p2 = pts[1]
+
+        // Calculates the distance
+        val inv = Geodesic.WGS84.Inverse(p1.x, p1.y, p2.x, p2.y)
+        val meters = inv.s12 // Distance in meters
+
+        return (meters * M_TO_MILES).toInt()
+    }
+
+    /**
+     * Helper function to convert a Geometry object to a GeoJsonMap.
+     * This is needed because Geometry objects are not automatically serialized to JSON.
+     *
+     * @param geometry The Geometry to convert
+     *
+     * @return The geometry converted to a GeoJSON format
+     */
+    private fun geometryToGeoJsonMap(geometry: Geometry): Map<String, Any?> {
+        val objectMapper = jacksonObjectMapper()
+        val geoJsonString = GeoJsonWriter().write(geometry) // valid for GeometryCollection too
+        println(objectMapper.readValue(geoJsonString, Map::class.java))
+        @Suppress("UNCHECKED_CAST")
+        return objectMapper.readValue(geoJsonString, Map::class.java) as Map<String, Any?>
     }
 }
